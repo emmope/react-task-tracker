f5d7779490216e590eff8559095db3890607444d
emmope <emmope@gmail.com>
2023-05-28 07:14:43 +0100

    second update

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,28 @@
-//  import {FaTimes} from 'react-icons'
+import PropTypes from 'prop-types';
+import { FaTrash } from 'react-icons/fa';
 
-const Task = (prop) => {
+const Task =({ task, onDelete, onToggle  }) => {
   return (
-    <div className='task'>
-        <h3>
-        {prop.text} 
-        </h3>
-        <p>
-        {prop.day}
-        </p>
-
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+      <h3>
+        {task.text} <FaTrash style={{ color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}  />
+      </h3>
+      <p>{task.day}</p>
     </div>
   )
 }
+Task.propTypes = {
+  task: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    day: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    reminder: PropTypes.bool.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onToggle: PropTypes.func.isRequired,
+}
 
-export default Task
 
+export default Task;
+
+  
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,27 @@
-import Task from "./Task"
+import PropTypes from 'prop-types';
+import Task from './Task';
 
-const Tasks = (prop) => {
-  const {tasks} = prop
-
-  return (  
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
     <>
-      {tasks.map((elem) => (
-      <Task key={elem.index} text={elem.text} day={elem.day}/>
-        ))}
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Tasks
+Tasks.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      day: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onToggle: PropTypes.func.isRequired,
+};
 
-
-
-
-
+export default Tasks;
 
